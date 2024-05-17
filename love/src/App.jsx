11e9ac93634e08755206fd26c41db26650c8216d
isaac <imisaacgonzalez@gmail.com>
2024-05-17 01:38:16 -0400

    Changes: Firebase User creation works / (REQ ENV FIXES) stopping for the night

--- love/src/App.jsx
+++ love/src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/" element={<Login/>} />
-      <Route path="/" element={<Signup/>} />
+      <Route path="/signup" element={<Signup/>} />
       </Routes>
     </div>
   )
--- love/src/components/signup/Signup.jsx
+++ love/src/components/signup/Signup.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase"
+import { useState } from "react";
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+      console.log('hi')
+    });
   return (
-    <div>
-      Signup
+    <div className="signup">
+      <h3>CREATE ACCOUNT</h3>
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
     </div>
   )
 }
+
