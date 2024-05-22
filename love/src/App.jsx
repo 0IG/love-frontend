808462114dbc11e49610c383597f115eafa2859c
Isaac Gonzalez <u1337x@gmail.com>
2024-05-22 15:26:43 -0400

    Intro: Indroduced Google Maps API / simple setup + small changes to Photos

--- love/src/App.jsx
+++ love/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 import Photos from "./components/photos/Photos";
+import Maps from "./components/maps/Maps";
 
 import { Routes, Route } from "react-router-dom";
 
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/photos" element={<Photos />} />
+        <Route path="/maps" element={<Maps />} />
       </Routes>
     </div>
   );
