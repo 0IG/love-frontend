a37c6dce256bf5c8645e5247a18f7403f7426de3
Isaac Gonzalez <u1337x@gmail.com>
2024-06-05 15:42:56 -0400

    Changes: Basic photo placeholder

--- love/src/components/photos/Photos.jsx
+++ love/src/components/photos/Photos.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Photo from "../photo/Photo";
+import axios from "axios";
 
 export default function Photos() {
+  const [photos, setPhotos] = useState([]);
+  const API = import.meta.env.VITE_LOCAL_API;
+
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((response) => {
+        setPhotos(response.data.payload);
+      })
+      .catch((error) => {
+        console.message(error);
+      });
+  }),
+    [];
+
   return (
     <div className="photos">
-      <Photo />
+      <div className="photos__container">
+        <Photo />
+      </div>
     </div>
   );
 }
